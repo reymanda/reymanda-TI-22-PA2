4f3896ab7385dabf505a74f49f6d4a3f1bac0b52
reymanda <113004517+reymanda@users.noreply.github.com>
2022-09-13 14:29:37 +0700

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 5; i++){ cout<<"belajar koding di kelas algoritma"<<endl;
+
+    
+    }
+  return 0;
+  
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 0;
+  do{
+    printf( "belajar koding di kelas algoritma \n");
+    i++;
+    }while(i < 5);
+  
+  return 0;
+}
